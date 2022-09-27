4813371752a18d3d0f6c7008fce5fa8198608fca
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2022-09-27 11:29:26 -0600

    Added current data structures to index file.

---
+++ index.js
@@ -0,0 +1,13 @@
+const Stack = require('./src/linear/stack/stack');
+const Queue = require('./src/linear/queue/queue');
+const Deque = require('./src/linear/deque/deque');
+const SLL = require('./src/linear/singlyLinkedList/singlyLinkedList');
+const SLLNode = require('./src/linear/singlyLinkedList/singlyLinkedListNode');
+
+module.exports = {
+  Stack,
+  Queue,
+  Deque,
+  SLL,
+  SLLNode,
+};
